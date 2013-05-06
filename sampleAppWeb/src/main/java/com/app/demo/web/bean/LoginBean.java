6d57b45064fa8633359482ccdcb6544f9ab7dd50
gshamsul@gmail.com <gshamsul@gmail.com@9819c009-ee1b-b27f-6bef-eb9d46cd4e3b>
2013-05-06 15:03:22 +0000

    update

--- sampleAppWeb/src/main/java/com/app/demo/web/bean/LoginBean.java
+++ sampleAppWeb/src/main/java/com/app/demo/web/bean/LoginBean.java
@@ -8,6 +8,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotEmpty;
@@ -28,6 +29,7 @@ public class LoginBean  implements Serializable {
 
 	private static final long serialVersionUID = 2868742783741899100L;
 	private Person person = new Person();
+	private Boolean isAdmin = false;
 	@NotEmpty
     @Size(min = 1, max = 25)
     private String userName;
@@ -48,14 +50,21 @@ public class LoginBean  implements Serializable {
     //ActionEvent actionEvent
 	public String login() throws java.io.IOException {
 		try {
-            Authentication request = new UsernamePasswordAuthenticationToken(this.getUserName(), getPassword());
-            Authentication result = am.authenticate(request);
+            Authentication req = new UsernamePasswordAuthenticationToken(this.getUserName(), getPassword());
+            Authentication result = am.authenticate(req);
             SecurityContextHolder.getContext().setAuthentication(result);
             person = (Person) SecurityContextHolder.getContext().getAuthentication().getDetails();
             System.out.println("Login Success! ..");
+            
+            HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+            
+            System.out.println("is Admin : "+request.isUserInRole("ROLE_ADMIN"));
+            isAdmin = request.isUserInRole("ROLE_ADMIN");
+            
             return "/pages/users?faces-redirect=true";
         } catch (AuthenticationException ex) {
         	System.out.println("Login Failed");
+        	ex.printStackTrace();
         	FacesContext.getCurrentInstance().addMessage("formLogin", new FacesMessage(FacesMessage.SEVERITY_WARN,"Login Failed", "User Name and Password Not Match!"));  
              
             return "/login";
@@ -105,4 +114,16 @@ public class LoginBean  implements Serializable {
 	public void setPerson(Person person) {
 		this.person = person;
 	}
+
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+
+	
+	
 }
